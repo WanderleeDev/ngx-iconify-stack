86ea7703669afbcd6e7d6c52320122ba32c6bba8
wanderlee <wanderleedev@gmail.com>
2026-08-14 00:02:42 -0500

    feat(docs): add docs link to navbar

--- projects/docs/src/app/components/navbar/navbar.component.ts
+++ projects/docs/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgxIconify } from 'ngx-iconify-stack';
 import { ThemeToggleService } from 'ngx-theme-stack';
 
 @Component({
   selector: 'docs-navbar',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgxIconify],
+  imports: [NgxIconify, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styles: ``,
+  styles: `
+    .nav-link-active {
+      color: var(--color-accent);
+    }
+  `,
 })
 export class NavbarComponent {
   protected readonly theme = inject(ThemeToggleService);
